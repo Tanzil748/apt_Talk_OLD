41124729380b81313cd5b5150ec5784f0b597ffb
Tanzil Hassan <tanzilhassan333@gmail.com>
2023-06-12 21:21:43 -0400

    User dropdown menu persists on refresh

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -3,15 +3,22 @@ import axios from "axios";
 
 const AuthContext = createContext();
 
+// loggedUser keeps track if user has a token
+// isLoggedIn keeps track of the user logged status (prevents dropdown menu from resetting on refresh)
+
 export const AuthContextProvider = ({ children }) => {
+  // make sure null is not read as a string or it wont work
   const [loggedUser, setLoggedUser] = useState(
     JSON.parse(localStorage.getItem("loggedUser")) || null
   );
-  const [isLoggedIn, setIsLoggedIn] = useState(false); //check if loggedIn for dropdown menu change
+  const [isLoggedIn, setIsLoggedIn] = useState(
+    JSON.parse(localStorage.getItem("menuChange")) || false
+  );
 
   useEffect(() => {
-    localStorage.setItem("loggedUser", JSON.stringify(loggedUser)); // loggedUser is an object, but values need to be string in local storage
-  }, [loggedUser]);
+    localStorage.setItem("loggedUser", JSON.stringify(loggedUser)); // loggedUser values need to be string in local storage
+    localStorage.setItem("menuChange", JSON.stringify(isLoggedIn));
+  }, [loggedUser, isLoggedIn]);
 
   const login = async (input) => {
     // get loggedUser info from backend and set it with 'setLoggedUser'
