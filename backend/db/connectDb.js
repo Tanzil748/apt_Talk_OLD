b7e59ee6ad3c8e71fb636196e629a51c376d496e
Tanzil Hassan <tanzilhassan333@gmail.com>
2023-07-01 19:38:47 -0400

    added local pw key

--- backend/db/connectDb.js
+++ backend/db/connectDb.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 export const pool = new Pool({
   user: "postgres",
-  password: process.env.DBPASS,
+  password: process.env.LOCAL_PW,
   host: "localhost",
   port: "5433",
   database: "aptTalk",
